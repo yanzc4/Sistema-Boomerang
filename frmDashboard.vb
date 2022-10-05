7f499509ac2887dbd1861d423f7d1413de4fb871
Yan ZC <yanzapatacardoza@gmail.com>
2022-10-05 13:52:20 -0500

    panel final

--- frmDashboard.vb
+++ frmDashboard.vb
@@ -2,4 +2,8 @@
     Private Sub ElContainer5_Click(sender As Object, e As EventArgs)
 
     End Sub
+
+    Private Sub ElContainer1_Click(sender As Object, e As EventArgs) Handles ElContainer1.Click
+
+    End Sub
 End Class
