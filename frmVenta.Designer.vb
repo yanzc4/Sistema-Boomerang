33e9b720695d7a86957f9a5bc2634c2d5f5a8034
Yan ZC <yanzapatacardoza@gmail.com>
2022-10-26 21:52:21 -0500

    creando form venta

--- frmVenta.Designer.vb
+++ frmVenta.Designer.vb
@@ -22,11 +22,22 @@ Partial Class frmVenta
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.ElContainer1 = New Klik.Windows.Forms.v1.EntryLib.ELContainer()
+        Me.dgvProductos = New System.Windows.Forms.DataGridView()
         Me.btnSalir = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.ElContainer7 = New Klik.Windows.Forms.v1.EntryLib.ELContainer()
+        Me.txtBuscar = New Sistema_Boomerang.PowerTextBox()
+        Me.btnBuscar = New Klik.Windows.Forms.v1.EntryLib.ELButton()
         CType(Me.ElContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.ElContainer1.SuspendLayout()
+        CType(Me.dgvProductos, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ElContainer7, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ElContainer7.SuspendLayout()
+        CType(Me.btnBuscar, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ElContainer1
@@ -37,6 +48,9 @@ Partial Class frmVenta
         Me.ElContainer1.BackgroundStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid
         Me.ElContainer1.BackgroundStyle.SolidColor = System.Drawing.Color.Black
         Me.ElContainer1.BorderStyle.SolidColor = System.Drawing.Color.FromArgb(CType(CType(81, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(81, Byte), Integer))
+        Me.ElContainer1.Controls.Add(Me.btnBuscar)
+        Me.ElContainer1.Controls.Add(Me.ElContainer7)
+        Me.ElContainer1.Controls.Add(Me.dgvProductos)
         Me.ElContainer1.Controls.Add(Me.btnSalir)
         Me.ElContainer1.Controls.Add(Me.Label1)
         Me.ElContainer1.Cursor = System.Windows.Forms.Cursors.Default
@@ -46,6 +60,48 @@ Partial Class frmVenta
         Me.ElContainer1.Size = New System.Drawing.Size(980, 646)
         Me.ElContainer1.TabIndex = 0
         Me.ElContainer1.VisualStyle = Klik.Windows.Forms.v1.Common.ControlVisualStyles.Custom
+        '
+        'dgvProductos
+        '
+        Me.dgvProductos.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.dgvProductos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.dgvProductos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.dgvProductos.BackgroundColor = System.Drawing.Color.Black
+        Me.dgvProductos.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.dgvProductos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        DataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle4.BackColor = System.Drawing.Color.Yellow
+        DataGridViewCellStyle4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        DataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black
+        DataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgvProductos.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle4
+        Me.dgvProductos.ColumnHeadersHeight = 30
+        Me.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.dgvProductos.EnableHeadersVisualStyles = False
+        Me.dgvProductos.GridColor = System.Drawing.Color.Yellow
+        Me.dgvProductos.Location = New System.Drawing.Point(89, 277)
+        Me.dgvProductos.Name = "dgvProductos"
+        Me.dgvProductos.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        DataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle5.BackColor = System.Drawing.Color.Yellow
+        DataGridViewCellStyle5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle5.ForeColor = System.Drawing.Color.Black
+        DataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        DataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.Black
+        DataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgvProductos.RowHeadersDefaultCellStyle = DataGridViewCellStyle5
+        Me.dgvProductos.RowHeadersWidth = 51
+        DataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(CType(CType(81, Byte), Integer), CType(CType(81, Byte), Integer), CType(CType(81, Byte), Integer))
+        DataGridViewCellStyle6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle6.ForeColor = System.Drawing.Color.White
+        DataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        DataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.Black
+        Me.dgvProductos.RowsDefaultCellStyle = DataGridViewCellStyle6
+        Me.dgvProductos.Size = New System.Drawing.Size(802, 217)
+        Me.dgvProductos.TabIndex = 118
         '
         'btnSalir
         '
@@ -72,6 +128,61 @@ Partial Class frmVenta
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Nueva Venta"
         '
+        'ElContainer7
+        '
+        Me.ElContainer7.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.ElContainer7.BackgroundStyle.GradientEndColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(227, Byte), Integer), CType(CType(3, Byte), Integer))
+        Me.ElContainer7.BackgroundStyle.GradientStartColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(142, Byte), Integer), CType(CType(9, Byte), Integer))
+        Me.ElContainer7.BackgroundStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid
+        Me.ElContainer7.BackgroundStyle.SolidColor = System.Drawing.Color.Black
+        Me.ElContainer7.BorderStyle.SolidColor = System.Drawing.Color.White
+        Me.ElContainer7.Controls.Add(Me.txtBuscar)
+        Me.ElContainer7.Cursor = System.Windows.Forms.Cursors.Default
+        Me.ElContainer7.Location = New System.Drawing.Point(345, 107)
+        Me.ElContainer7.Name = "ElContainer7"
+        Me.ElContainer7.Padding = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.ElContainer7.Size = New System.Drawing.Size(274, 34)
+        Me.ElContainer7.TabIndex = 120
+        Me.ElContainer7.VisualStyle = Klik.Windows.Forms.v1.Common.ControlVisualStyles.Custom
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.BackColor = System.Drawing.Color.Black
+        Me.txtBuscar.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.txtBuscar.ForeColor = System.Drawing.Color.White
+        Me.txtBuscar.Location = New System.Drawing.Point(8, 7)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(259, 20)
+        Me.txtBuscar.TabIndex = 1
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.btnBuscar.BackgroundImageStyle.FilterColor = System.Drawing.Color.Black
+        Me.btnBuscar.BackgroundImageStyle.Image = Global.Sistema_Boomerang.My.Resources.Resources.buscar1
+        Me.btnBuscar.BackgroundImageStyle.ImageAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.btnBuscar.BackgroundImageStyle.ImageSize = New System.Drawing.Size(24, 24)
+        Me.btnBuscar.BackgroundStyle.GradientEndColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(227, Byte), Integer), CType(CType(3, Byte), Integer))
+        Me.btnBuscar.BackgroundStyle.GradientStartColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(142, Byte), Integer), CType(CType(9, Byte), Integer))
+        Me.btnBuscar.BorderStyle.SolidColor = System.Drawing.Color.Black
+        Me.btnBuscar.Cursor = System.Windows.Forms.Cursors.Default
+        Me.btnBuscar.FlashStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid
+        Me.btnBuscar.FlashStyle.SolidColor = System.Drawing.Color.FromArgb(CType(CType(253, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(191, Byte), Integer))
+        Me.btnBuscar.ForegroundImageStyle.ImageAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.btnBuscar.Location = New System.Drawing.Point(625, 107)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(43, 34)
+        Me.btnBuscar.StateStyles.HoverStyle.BackgroundGradientEndColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(227, Byte), Integer), CType(CType(3, Byte), Integer))
+        Me.btnBuscar.StateStyles.HoverStyle.BackgroundGradientStartColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(227, Byte), Integer), CType(CType(3, Byte), Integer))
+        Me.btnBuscar.StateStyles.HoverStyle.BorderGradientStartColor = System.Drawing.Color.Black
+        Me.btnBuscar.StateStyles.HoverStyle.TextForeColor = System.Drawing.Color.Black
+        Me.btnBuscar.TabIndex = 121
+        Me.btnBuscar.TextStyle.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBuscar.TextStyle.ForeColor = System.Drawing.Color.White
+        Me.btnBuscar.TextStyle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.btnBuscar.VisualStyle = Klik.Windows.Forms.v1.EntryLib.ButtonVisualStyles.Custom
+        '
         'frmVenta
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -86,6 +197,11 @@ Partial Class frmVenta
         CType(Me.ElContainer1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ElContainer1.ResumeLayout(False)
         Me.ElContainer1.PerformLayout()
+        CType(Me.dgvProductos, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ElContainer7, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ElContainer7.ResumeLayout(False)
+        Me.ElContainer7.PerformLayout()
+        CType(Me.btnBuscar, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -93,4 +209,8 @@ Partial Class frmVenta
     Friend WithEvents ElContainer1 As Klik.Windows.Forms.v1.EntryLib.ELContainer
     Friend WithEvents btnSalir As Label
     Friend WithEvents Label1 As Label
+    Friend WithEvents dgvProductos As DataGridView
+    Friend WithEvents ElContainer7 As Klik.Windows.Forms.v1.EntryLib.ELContainer
+    Friend WithEvents txtBuscar As PowerTextBox
+    Friend WithEvents btnBuscar As Klik.Windows.Forms.v1.EntryLib.ELButton
 End Class
--- frmVenta.vb
+++ frmVenta.vb
@@ -1,6 +1,20 @@
-﻿Public Class frmVenta
-
+﻿Imports System.Data.SqlClient
+Public Class frmVenta
+    Private Sub Poblar()
+        Dim da As New SqlDataAdapter("select p.id CODIGO, p.f_plato IMAGEN, p.nombre MENU, c.nombre CATEGORIA, p.precio PRECIO from platos p join categoria_p c on p.id_categoria=c.id_categoria where p.nombre like'%" & txtBuscar.Text & "%'", conexion.conexion)
+        Dim ds As New DataSet
+        da.Fill(ds)
+        If ds.Tables(0).Rows.Count > 0 Then
+            dgvProductos.DataSource = ds.Tables(0)
+        Else
+            dgvProductos.DataSource = Nothing
+        End If
+    End Sub
     Private Sub btnSalir_Click_1(sender As Object, e As EventArgs) Handles btnSalir.Click
         Me.Close()
+    End Sub
+
+    Private Sub frmVenta_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Poblar()
     End Sub
 End Class
